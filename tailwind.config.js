08fc01fdad4db7aadf697943f2fbcab41101f54d
Marianela Landoni <mlandonih@hotmail.com>
2024-11-13 19:19:30 +0100

    feat: add colors in tailwind config and font family to body in styles.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
+    colors: {
+      primary: "#0158a3",
+      "primary-hover": "#064b86",
+      "primary-focus": "#0b3f6f",
+      secondary: "#00d8ab",
+      white: "#fff",
+      black: "#000b14",
+      "dark-text": "#25272c",
+      error: '#fc0606',
+      success: '#3db500'
+    },
     extend: {},
   },
   plugins: [],
