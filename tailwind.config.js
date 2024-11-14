3673e482a1bedeb487fef2c956079272af5682e3
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-11-14 13:40:39 +0100

    esquema formulario

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",],
+  content: ["./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     colors: {
       primary: "#0158a3",
