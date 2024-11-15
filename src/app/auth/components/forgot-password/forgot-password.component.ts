08ffe8bcde00b5ade12810efe6300a88b91153f1
Marianela Landoni <mlandonih@hotmail.com>
2024-11-15 14:08:38 +0100

    feat: add success section to forgot-password

--- src/app/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/auth/components/forgot-password/forgot-password.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonComponent } from '../../../core/components/button/button.component';
+import { HeaderComponent } from '../../../core/components/header/header.component';
+
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ButtonComponent, HeaderComponent],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css',
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit{
   resetPasswordForm!: FormGroup;
+  showSuccessSection: boolean = false;
 
 
   constructor(private fb: FormBuilder) {}
@@ -30,6 +34,8 @@ export class ForgotPasswordComponent {
       const email = this.resetPasswordForm.get('email')?.value;
       console.log('Restablecer contraseña para:', email);
       // Lógica para enviar el correo de restablecimiento de contraseña
+      //Si se ha enviado todo ok, mostrar sección de se ha enviado el correo para restablecer contraseña
+      this.showSuccessSection = true;
     }
   }
 
