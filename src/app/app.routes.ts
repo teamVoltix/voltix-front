4c9ec0343ec147b1642e2db59ee6d7deb78fcfc5
bertomeu <josecarlosbertomeu@gmail.com>
2024-11-17 20:15:57 +0000

    Añadido comoponente home y login Añadido carpetas de iconos e imagenes para importacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { HomeComponent } from './auth/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'login',
+    title: 'My Login Page',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    title: 'My Home Page',
+    component: HomeComponent,
+  }
+];
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  passwordVisible: boolean = false; // Controla la visibilidad de la contraseña
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible; // Alterna la visibilidad
+  }
 
 }
