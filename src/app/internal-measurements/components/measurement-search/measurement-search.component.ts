8fa90eefcc2c4ab78ea13d7f6a55280f9a6f470a
Marianela Landoni <mlandonih@hotmail.com>
2024-11-25 13:12:51 +0100

    feat: add measurement compare component

---
+++ src/app/internal-measurements/components/measurement-search/measurement-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './measurement-search.component.html',
+  styleUrl: './measurement-search.component.css'
+})
+export class MeasurementSearchComponent {
+
+}
