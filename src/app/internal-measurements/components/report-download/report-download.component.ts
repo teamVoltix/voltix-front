af28dd1ed940cef816e8891ea6f62a851c47a031
Marianela Landoni <mlandonih@hotmail.com>
2024-11-25 13:13:06 +0100

    feat: add report download component

---
+++ src/app/internal-measurements/components/report-download/report-download.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-download',
+  standalone: true,
+  imports: [],
+  templateUrl: './report-download.component.html',
+  styleUrl: './report-download.component.css'
+})
+export class ReportDownloadComponent {
+
+}
