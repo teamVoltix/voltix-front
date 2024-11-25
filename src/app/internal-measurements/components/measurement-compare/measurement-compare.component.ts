c6c540f563c7e93ab30a6760f97591bbe6832e5d
Marianela Landoni <mlandonih@hotmail.com>
2024-11-25 13:12:34 +0100

    feat: add measurement compare component

---
+++ src/app/internal-measurements/components/measurement-compare/measurement-compare.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-compare',
+  standalone: true,
+  imports: [],
+  templateUrl: './measurement-compare.component.html',
+  styleUrl: './measurement-compare.component.css'
+})
+export class MeasurementCompareComponent {
+
+}
