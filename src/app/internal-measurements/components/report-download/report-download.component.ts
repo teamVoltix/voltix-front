14acd4c2ba444807e2bdda5f41f164ec678826b4
Marianela Landoni <mlandonih@hotmail.com>
2024-11-26 14:56:35 +0100

    feat: add reportDownload modal

--- src/app/internal-measurements/components/report-download/report-download.component.ts
+++ src/app/internal-measurements/components/report-download/report-download.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { ReportService } from '../../services/report-service/report.service';
 
 @Component({
   selector: 'app-report-download',
   standalone: true,
   imports: [],
   templateUrl: './report-download.component.html',
-  styleUrl: './report-download.component.css'
+  styleUrl: './report-download.component.css',
 })
 export class ReportDownloadComponent {
+  constructor(private reportService: ReportService) {}
 
+  ngAfterViewInit() {
+    this.reportService.initializeModal();
+  }
+
+  showModal() {
+    this.reportService.showModal();
+  }
+
+  hideModal() {
+    this.reportService.hideModal();
+  }
+
+  downloadReport() {
+    console.log('Descargar');
+  }
 }
