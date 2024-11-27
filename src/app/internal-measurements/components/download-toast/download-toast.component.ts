512899515fe657bb20a5972d114c8df9e4102450
Marianela Landoni <mlandonih@hotmail.com>
2024-11-27 13:28:48 +0100

    fix: add setTimeOut and downloadFinished to closeToast method

--- src/app/internal-measurements/components/download-toast/download-toast.component.ts
+++ src/app/internal-measurements/components/download-toast/download-toast.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { ReportService } from '../../services/report-service/report.service';
 
 @Component({
   selector: 'app-download-toast',
@@ -13,7 +14,13 @@ export class DownloadToastComponent {
   pdfName = '546846.PDF';
   percentage = 80
 
+  constructor(protected reportService: ReportService){}
+
   closeToast() {
     this.isToastExiting = true;
+
+    setTimeout(() => {
+      this.reportService.downloadFinished = false;
+    }, 400);
   }
 }
