ea6d74dc79ad11259d4133f23271e27ac48cc862
daviniapd <delgadavi@gmail.com>
2024-11-29 17:39:04 +0100

    home page ux/ui

--- src/app/auth/components/home-page/home-page.component.ts
+++ src/app/auth/components/home-page/home-page.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Router } from '@angular/router';
+import Swiper from 'swiper/bundle'; // La importación correcta si has instalado Swiper en la última versión
+import { Navigation } from 'swiper/modules'; // Importa el módulo de navegación
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'] 
+  styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
+  
   user = {
-    firstname: 'Juan' // Quemado mientras no se conecte el backend, despues hacer dinamico
+    firstname: 'Juan' // Temporario hasta conectarse con backend
   };
+
+  constructor(private location: Location, private router: Router) {}
+
+  goToUploadInvoice(): void {
+    this.router.navigate(['/invoice-upload']); 
+  }
+
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper-container', {
+      modules: [Navigation],
+      slidesPerView: 1, 
+      spaceBetween: 10,
+      
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true, 
+      },
+    
+      breakpoints: {
+        640: {
+          slidesPerView: 1, 
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        850: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4, 
+        },
+      },
+    });
+  }
 }
