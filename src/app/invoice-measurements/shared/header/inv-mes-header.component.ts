dfd487fffd263f700b0cefb178636b3abb103b70
Marianela Landoni <mlandonih@hotmail.com>
2024-12-02 10:53:34 +0100

    feat: update inv-mes-header

--- src/app/invoice-measurements/shared/header/inv-mes-header.component.ts
+++ src/app/invoice-measurements/shared/header/inv-mes-header.component.ts
@@ -1,27 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-invoice-header',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './inv-mes-header.component.html',
-  styleUrl: './inv-mes-header.component.css'
+  styleUrl: './inv-mes-header.component.css',
 })
-export class InvMesHeaderComponent {
-  router=inject(Router)
-  selectedTab: string = 'facturas'; // Por defecto, selecciona 'Facturas'
+export class InvMesHeaderComponent implements OnInit {
+  router = inject(Router);
+  selectedTab: string = '';
+
+  ngOnInit() {
+    if (this.currentRoute.includes('invoce-listing')) {
+      this.selectedTab = 'facturas';
+    } else if (this.currentRoute.includes('measurement')) {
+      this.selectedTab = 'mediciones';
+    }
+  }
+
+  get currentRoute() {
+    return this.router.url;
+  }
+
+  get showBackArrow(): boolean {
+    return (
+      this.currentRoute === '/measurement-compare' ||
+      this.currentRoute.match(/^\/measurement-search\/\d+$/) !== null
+    );
+  }
 
   selectTab(tab: string): void {
     this.selectedTab = tab;
+    const route =
+      tab === 'facturas' ? '/invoce-listing' : '/measurement-search';
+    this.router.navigate([route]);
   }
-  invoice(){
-    this.router.navigate([`/invoce-listing`])
-  }
-  medicion(){
-    this.router.navigate([`/start`])
+
+  goToMeasurementSearch() {
+    this.router.navigate(['/measurement-search']);
   }
 
 }
