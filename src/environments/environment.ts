70883c41401885c0ef5dae3a7d93a416d12516de
Angie <eguzkia6@gmail.com>
2024-12-10 15:12:51 +0100

    changed environment for develop

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
-// export const environment = {
-//   API_URL: 'http://ec2-3-81-229-130.compute-1.amazonaws.com/',
-// };
-
-//localhost
 export const environment = {
-  API_URL: 'http://127.0.0.1:8000/',
+  API_URL: 'http://ec2-3-81-229-130.compute-1.amazonaws.com/',
 };
+
