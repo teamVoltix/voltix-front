61385086f9f91b4aa1569d47400e7314c33dca1c
Angie <eguzkia6@gmail.com>
2024-12-10 14:56:52 +0100

    added localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 // export const environment = {
 //   API_URL: 'http://ec2-3-81-229-130.compute-1.amazonaws.com/',
 // };
+
+//localhost
 export const environment = {
-  API_URL: 'http://ec2-3-81-229-130.compute-1.amazonaws.com/',
+  API_URL: 'http://127.0.0.1:8000/',
 };
