ff85fc932f9f6f4c5b02c7e1895411eef3ac73ff
Denis <denis9diaz@hotmail.com>
2024-12-12 13:14:17 +0100

    modificado login, botones de comparar y pagina de inicio

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent {
   // Alterna la visibilidad del campo de contraseña
   togglePasswordVisibility(): void {
     this.passwordVisible = !this.passwordVisible;
-  }
+  }  
 
   // Actualiza el estado general de los campos
   actualizarEstadoCampos(): void {
