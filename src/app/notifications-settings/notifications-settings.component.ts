b02eab0b2fc9e9e32b33c590681c79c02cbe39f5
Angie <eguzkia6@gmail.com>
2025-01-21 20:44:46 +0100

    update notification recommendations

--- src/app/notifications-settings/notifications-settings.component.ts
+++ src/app/notifications-settings/notifications-settings.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { HomepageService } from '../home-page/service/homepage.service'; // Ajusta esta ruta si es necesario
 import { User } from '../core/model/user'; // Ajusta esta ruta si es necesario
+import { NotificationService } from '../notifications/service/notification.service';
 
 @Component({
   selector: 'app-notifications-settings',
@@ -13,14 +14,16 @@ import { User } from '../core/model/user'; // Ajusta esta ruta si es necesario
 })
 export class NotificationsSettingsComponent implements OnInit {
   user!: User; // Información del usuario
-
+  notificationSettings: any = {};
+  private notificationService = inject(NotificationService);
   private homepageService = inject(HomepageService); // Inyección del servicio de perfil
   private router = inject(Router);
 
   constructor() {}
 
   ngOnInit(): void {
-    this.loadUserProfile(); // Cargar el perfil del usuario al iniciar
+    this.loadUserProfile();
+    this.loadNotificationSettings();
   }
 
   // Cargar el perfil del usuario desde el servicio
@@ -44,4 +47,27 @@ export class NotificationsSettingsComponent implements OnInit {
   get showBackArrow(): boolean {
     return true; // Mostrar siempre la flecha de retroceso en esta vista
   }
+
+  loadNotificationSettings(): void {
+    this.notificationService.getNotificationSettings().subscribe({
+      next: (settings) => {
+        this.notificationSettings = settings.data; // Assuming the backend returns settings in a `data` field
+      },
+      error: (error: any) => {
+        console.error('Error loading notification settings:', error);
+      },
+    });
+  }
+
+  updateNotificationSettings(): void {
+    this.notificationService.updateNotificationSettings(this.notificationSettings).subscribe({
+      next: (response) => {
+        console.log('Notification settings updated:', response);
+      },
+      error: (error: any) => {
+        console.error('Error updating notification settings:', error);
+      },
+    });
+  }
+  
 }
